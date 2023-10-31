02d0c5d6a98b51f7830bd050b4044502b40d70af
wallpants <47203170+wallpants@users.noreply.github.com>
2023-10-30 19:10:04 -0600

    fix(vim.log.levels): export NVIM_LOG_LEVELS const

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 export { attach } from "./attach.ts";
 export * from "./types.ts";
+
+export const NVIM_LOG_LEVELS = {
+    TRACE: 0,
+    DEBUG: 1,
+    INFO: 2,
+    WARN: 3,
+    ERROR: 4,
+    OFF: 5,
+} as const;
